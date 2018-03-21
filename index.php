8192feb78088e1f4f34eefdd44f3bd5b62b21ae4
Kachorroski <kernelkill@outlook.com>
2018-03-20 21:09:21 -0400

    deu erro mais foi

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Meu Git</title>
+</head>
+<body>
+
+<h1>Aprendendo GIT</h1>
+
+<p>GIT é a melhor ferramenta</p>
+	
+</body>
+</html>
